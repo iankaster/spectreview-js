9158393fdf9f7dc40813cd9ca57a72d45ab4abda
Ian Kaster <ikaster@gmail.com>
2013-03-19 21:36:57 -0400

    refactoring jdxreader.js changes

--- js/spectreview.jdxreader.js
+++ js/spectreview.jdxreader.js
@@ -1,198 +1,34 @@
-(function ( SpectreView, $, undefined ) {
+(function ( SpectreView, undefined ) {
 
-	SpectreView.Type = {
-		UNKNOWN : 0,
-		INFRARED_SPECTRUM : 1,
-		NMR_SPECTRUM_13C : 2,
-		NMR_SPECTRUM_1H : 3
-	}
-
-	SpectreView.JdxReader = function () {
-
-		var raw;
-		var metaData;
-		var xyData;
-		var points;
+	SpectreView.Jdx = {};
+	
+	SpectreView.Jdx.Decompressor = function (xFactor, yFactor, deltaX) {
+		var self = this;
 		
-		var pointsInTransmittance, 
-			pointsInAbsorbance, 
-			integrationPoints,
-			pointsInHz,
-			pointsInPpm;
+		this.decompress = function (line) {
+			var tokens = self.split(line);
+			tokens = self.process(tokens);
+		
+			var points = self.compute(tokens);
 			
-		this.load = function (data) {
-			raw = data;
+			return points;
+		};
 		
-			parseMetaData();
-			processMetaData();
-			
-			parseXyData();
-			processXyData();
-		}
+		this.compute = function (tokens) {
+			var x = tokens.splice(0, 1) * xFactor;
 		
-		this.getType = function () {
-			var dataType = metaData['DATA TYPE'].toUpperCase();
-			var observerNucleus = metaData['.OBSERVE NUCLEUS'];
-		
-			switch (true) {
-				case dataType == 'INFRARED SPECTRUM':
-					return SpectreView.Type.INFRARED_SPECTRUM;
+			var points = [];
+			for (var i = 0, length = tokens.length; i < length; i++) {
+				var y = tokens[i] * yFactor;
 					
-				case dataType == 'NMR SPECTRUM' && observerNucleus == '1H':
-					return SpectreView.Type.NMR_SPECTRUM_1H;
-				
-				case dataType == 'NMR SPECTRUM' && observerNucleus == '13C':
-					return SpectreView.Type.NMR_SPECTRUM_13C;
-					
-				default:
-					return SpectreView.Type.UNKNOWN;
-			}
-		}
-		
-		this.getPoints = function () {
+				points.push([x, y]);
+				x += deltaX;
+			}	
+
 			return points;
 		}
 		
-		this.getPointsInTransmittance = function () {
-			if (!pointsInTransmittance) {
-				if (metaData['YUNITS'] == 'ABSORBANCE') {
-					pointsInTransmittance = [];
-
-					$.each(points, function (index, point) {
-						pointsInTransmittance.push([point[0], Math.pow(10, -1 * point[1])])
-					});
-				}
-				else if (metaData['YUNITS'] == 'TRANSMITTANCE') {
-					pointsInTransmittance = points;
-				}
-			}
-			
-			return pointsInTransmittance;
-		}
-			
-		this.getPointsInAbsorbance = function () {
-			if (!pointsInAbsorbance) {
-				if (metaData['YUNITS'] == 'TRANSMITTANCE') {
-					pointsInAbsorbance = [];
-				
-					$.each(points, function (index, point) {
-						pointsInAbsorbance.push([point[0], point[1] == 0 ? 0 : Math.log(1 / Math.abs(point[1])) / Math.LN10])
-					});
-				}
-				else if (metaData['YUNITS'] == 'ABSORBANCE') {
-					pointsInAbsorbance = points;
-				}
-			}
-			
-			return pointsInAbsorbance;
-		}
-		
-		this.getIntegrationPoints = function () {
-			if (!integrationPoints) {
-				// TODO: may need to add an additional check against metaData['INSTRUMENTAL PARAMETERS'] ??
-				if (metaData['.OBSERVE NUCLEUS'] == '1H') {
-					integrationPoints = [];
-					
-					var value = Math.abs(metaData['MAXY'] - metaData['MINY']) * 0.15;
-					var threshold = Math.abs(metaData['MAXY'] - metaData['MINY']) * 0.05;
-					
-					$.each(points, function (index, point) {
-						if (point[1] > threshold) {
-							value += point[1] * 0.025;
-						}
-						
-						integrationPoints.push([point[0], value]);
-					});
-				}
-			}
-			
-			return integrationPoints;
-		}
-		
-		this.getPointsInHz = function () {
-			if (!pointsInHz) {
-				if (metaData['XUNITS'] == 'PPM') {
-					pointsInHz = [];
-					
-					$.each(points, function (index, point) {
-						pointsInHz.push([point[0] * metaData['.OBSERVE FREQUENCY'], point[1]]);
-					});
-				}
-				else if (metaData['XUNITS'] == 'HZ') {
-					pointsInHz = points;
-				}
-			}
-			
-			return pointsInHz;
-		}
-		
-		this.getPointsInPpm = function () {
-			if (!pointsInPpm) {
-				if (metaData['XUNITS'] == 'HZ') {
-					pointsInPpm = [];
-					
-					$.each(points, function (index, point) {
-						pointsInPpm.push([point[0] / metaData['.OBSERVE FREQUENCY'], point[1]]);
-					});
-				}
-				else if (metaData['XUNITS'] == 'PPM') {
-					pointsInPpm = points;
-				}
-			}
-			
-			return pointsInPpm;
-		}
-		
-		function parseMetaData () {
-			// TODO: update this pattern to exclude trailing white space
-			var pattern = /^##(.*)=\s*(.*)$/igm,
-				match;
-			
-			metaData = {};
-			while ( match = pattern.exec(raw) )
-				metaData[match[1]] = match[2];
-		}
-		
-		function processMetaData () {
-			metaData['XFACTOR'] = parseFloat(metaData['XFACTOR']);
-			metaData['YFACTOR'] = parseFloat(metaData['YFACTOR']);
-			metaData['LASTX'] = parseFloat(metaData['LASTX']);
-			metaData['FIRSTX'] = parseFloat(metaData['FIRSTX']);
-			metaData['NPOINTS'] = parseFloat(metaData['NPOINTS']);
-			metaData['MAXY'] = parseFloat(metaData['MAXY']);
-			metaData['MINY'] = parseFloat(metaData['MINY']);
-			metaData['.OBSERVE FREQUENCY'] = parseFloat(metaData['.OBSERVE FREQUENCY']);
-		
-			metaData['DELTAX'] = metaData['DELTAX'] ? parseFloat(metaData['DELTAX']) : ( metaData['LASTX'] - metaData['FIRSTX'] ) / ( metaData['NPOINTS'] - 1 );
-		}
-		
-		function parseXyData () {
-			var pattern = /^##XYDATA=(.*)$\n([\s\S]*)^##END=.*$/igm;
-			var match = pattern.exec(raw);
-			
-			xyData = match[2].split('\n');
-		}
-		
-		function processXyData () {
-			points = [];
-
-			$.each(xyData, function (index, line) {
-				var tokens = splitLine(line);
-				
-				tokens = processLine(tokens);
-				
-				var x = tokens.splice(0, 1) * metaData['XFACTOR'];
-				
-				$.each(tokens, function (index, token) {
-					var y = token * metaData['YFACTOR'];
-					
-					points.push([x, y]);
-					x += metaData['DELTAX'];
-				});
-			});
-		}
-		
-		this.splitLine = function (line) {
+		this.split = function (line) {
 			var pattern = /(-?[\d\.]+|[a-z@%])[\s\+]*/ig,
 				match;
 			
@@ -200,22 +36,25 @@
 			while ( match = pattern.exec(line) )
 				tokens.push(match[1]);
 				
-			return tokens;
-		}
+			return tokens;		
+		};
 		
-		this.processLine = function (tokens) {
+		this.process = function (tokens) {
+			// TODO: can these be combined into a single loop
 			tokens = processDuplication(tokens);
 			tokens = processSqueeze(tokens);
 			tokens = processDifference(tokens);
 			
 			return tokens;
-		}
-		
+		};
+
 		function processDuplication (tokens) {
 			var pattern = /^[S-Zs]$/,
 				decompressed = [];
 			
-			$.each(tokens, function (index, token) {
+			for (var index = 0, length = tokens.length; index < length; index++) {
+				var token = tokens[index];
+				
 				switch (true) {
 					case pattern.test(token):
 						var duplicate = tokens[index - 1];
@@ -229,7 +68,7 @@
 					default:
 						decompressed.push(token);
 				}
-			});
+			}
 			
 			return decompressed;
 		}
@@ -239,7 +78,7 @@
 				negativePattern = /^[a-i]/,
 				decompressed = [];
 				
-			for (var index = 0; index < tokens.length; index++) {
+			for (var index = 0, length = tokens.length; index < length; index++) {
 				var token = tokens[index];
 				
 				switch (true) {
@@ -280,7 +119,7 @@
 				negativeDifPattern = /^[j-r]/,
 				decompressed = [];
 
-			for (var index = 0; index < tokens.length; index++) {
+			for (var index = 0, length = tokens.length; index < length; index++) {
 				var token = tokens[index];
 			
 				switch (true) {
@@ -316,7 +155,202 @@
 			}
 			
 			return decompressed;
-		}			
+		}	
+	
 	}
 
-}( window.SpectreView = window.SpectreView || {}, jQuery));
+	SpectreView.Jdx.Type = {
+		UNKNOWN : 0,
+		INFRARED_SPECTRUM : 1,
+		NMR_SPECTRUM_13C : 2,
+		NMR_SPECTRUM_1H : 3
+	}
+
+	SpectreView.Jdx.Reader = function () {
+
+		var raw,
+			metaData,
+			xyData,
+			
+			points,
+			pointsInTransmittance, 
+			pointsInAbsorbance, 
+			integrationPoints,
+			pointsInHz,
+			pointsInPpm;
+			
+		this.load = function (data) {
+			raw = data;
+		
+			parseMetaData();
+			processMetaData();
+			
+			parseXyData();
+			processXyData();
+		}
+		
+		this.getType = function () {
+			var dataType = metaData['DATA TYPE'].toUpperCase();
+			var observerNucleus = metaData['.OBSERVE NUCLEUS'];
+		
+			switch (true) {
+				case dataType == 'INFRARED SPECTRUM':
+					return SpectreView.Jdx.Type.INFRARED_SPECTRUM;
+					
+				case dataType == 'NMR SPECTRUM' && observerNucleus == '1H':
+					return SpectreView.Jdx.Type.NMR_SPECTRUM_1H;
+				
+				case dataType == 'NMR SPECTRUM' && observerNucleus == '13C':
+					return SpectreView.Jdx.Type.NMR_SPECTRUM_13C;
+					
+				default:
+					return SpectreView.Jdx.Type.UNKNOWN;
+			}
+		}
+		
+		this.getPoints = function () {
+			return points;
+		}
+		
+		this.getPointsInTransmittance = function () {
+			if (!pointsInTransmittance) {
+				if (metaData['YUNITS'] == 'ABSORBANCE') {
+					pointsInTransmittance = [];
+
+					for (var i = 0, length = points.length; i < length; i++) {
+						var point = points[i];
+						
+						pointsInTransmittance.push([point[0], Math.pow(10, -1 * point[1])])
+					}
+				}
+				else if (metaData['YUNITS'] == 'TRANSMITTANCE') {
+					pointsInTransmittance = points;
+				}
+			}
+			
+			return pointsInTransmittance;
+		}
+			
+		this.getPointsInAbsorbance = function () {
+			if (!pointsInAbsorbance) {
+				if (metaData['YUNITS'] == 'TRANSMITTANCE') {
+					pointsInAbsorbance = [];
+				
+					for (var i = 0, length = points.length; i < length; i++) {
+						var point = points[i];
+						
+						pointsInAbsorbance.push([point[0], point[1] == 0 ? 0 : Math.log(1 / Math.abs(point[1])) / Math.LN10])
+					}
+				}
+				else if (metaData['YUNITS'] == 'ABSORBANCE') {
+					pointsInAbsorbance = points;
+				}
+			}
+			
+			return pointsInAbsorbance;
+		}
+		
+		this.getIntegrationPoints = function () {
+			if (!integrationPoints) {
+				if (metaData['.OBSERVE NUCLEUS'] == '1H') {
+					integrationPoints = [];
+					
+					var value = Math.abs(metaData['MAXY'] - metaData['MINY']) * 0.15;
+					var threshold = Math.abs(metaData['MAXY'] - metaData['MINY']) * 0.05;
+					
+					for (var i = 0, length = points.length; i < length; i++) {
+						var point = points[i];
+						
+						if (point[1] > threshold) {
+							value += point[1] * 0.025;
+						}
+						
+						integrationPoints.push([point[0], value]);
+					}
+				}
+			}
+			
+			return integrationPoints;
+		}
+		
+		this.getPointsInHz = function () {
+			if (!pointsInHz) {
+				if (metaData['XUNITS'] == 'PPM') {
+					pointsInHz = [];
+					
+					for (var i = 0, length = points.length; i < length; i++) {
+						var point = points[i];
+						
+						pointsInHz.push([point[0] * metaData['.OBSERVE FREQUENCY'], point[1]]);
+					}
+				}
+				else if (metaData['XUNITS'] == 'HZ') {
+					pointsInHz = points;
+				}
+			}
+			
+			return pointsInHz;
+		}
+		
+		this.getPointsInPpm = function () {
+			if (!pointsInPpm) {
+				if (metaData['XUNITS'] == 'HZ') {
+					pointsInPpm = [];
+					
+					for (var i = 0, length = points.length; i < length; i++) {
+						var point = points[i];
+						
+						pointsInPpm.push([point[0] / metaData['.OBSERVE FREQUENCY'], point[1]]);
+					}
+				}
+				else if (metaData['XUNITS'] == 'PPM') {
+					pointsInPpm = points;
+				}
+			}
+			
+			return pointsInPpm;
+		}
+		
+		function parseMetaData () {
+			// TODO: update this pattern to exclude trailing white space
+			var pattern = /^##(.*)=\s*(.*)$/igm,
+				match;
+			
+			metaData = {};
+			while ( match = pattern.exec(raw) )
+				metaData[match[1]] = match[2];
+		}
+		
+		function processMetaData () {
+			metaData['XFACTOR'] = parseFloat(metaData['XFACTOR']);
+			metaData['YFACTOR'] = parseFloat(metaData['YFACTOR']);
+			metaData['LASTX'] = parseFloat(metaData['LASTX']);
+			metaData['FIRSTX'] = parseFloat(metaData['FIRSTX']);
+			metaData['NPOINTS'] = parseFloat(metaData['NPOINTS']);
+			metaData['MAXY'] = parseFloat(metaData['MAXY']);
+			metaData['MINY'] = parseFloat(metaData['MINY']);
+			metaData['.OBSERVE FREQUENCY'] = parseFloat(metaData['.OBSERVE FREQUENCY']);
+		
+			metaData['DELTAX'] = metaData['DELTAX'] ? parseFloat(metaData['DELTAX']) : ( metaData['LASTX'] - metaData['FIRSTX'] ) / ( metaData['NPOINTS'] - 1 );
+		}
+		
+		function parseXyData () {
+			var pattern = /^##XYDATA=(.*)$\n([\s\S]*)^##END=.*$/igm;
+			var match = pattern.exec(raw);
+			
+			xyData = match[2].split('\n');
+		}
+		
+		function processXyData () {
+			var decompressor = new SpectreView.Jdx.Decompressor(metaData['XFACTOR'], metaData['YFACTOR'], metaData['DELTAX']);
+		
+			points = [];
+
+			for (var i = 0, length = xyData.length; i < length; i++) {
+				var decompressed = decompressor.decompress(xyData[i]);
+				points = points.concat(decompressed);
+			}
+		}
+	}
+
+}( window.SpectreView = window.SpectreView || {} );
--- test/spectreview.jdxreader.tests.js
+++ test/spectreview.jdxreader.tests.js
@@ -1,26 +1,36 @@
-var jdxReader;
+module('SpectreView.Jdx.Reader');
 
-module('SpectreView.JdxReader', {
-	setup: function () {
-		jdxReader = new SpectreView.JdxReader();
-	}
-});
+module('SpectreView.Jdx.Decompressor');
 
-test('splitLine', function () {
+test('split', function () {
+	var jdxDecompressor = new SpectreView.Jdx.Decompressor();
+
 	var line = '599.860@VKT%TLkj%J%KLJ%njKjL%kL%jJULJ%kLK1%lLMNPNPRLJ0QTOJ1P';
 	var expected = ['599.860','@','V','K','T','%','T','L','k','j','%','J','%','K','L','J','%','n','j','K','j','L','%','k','L','%','j','J','U','L','J','%','k','L','K','1','%','l','L','M','N','P','N','P','R','L','J','0','Q','T','O','J','1','P'];
 	
-	var tokens = jdxReader.splitLine(line);
+	var tokens = jdxDecompressor.split(line);
 	
 	deepEqual(tokens, expected);
 });
 
-test('processLine', function () {
+test('process', function () {
+	var jdxDecompressor = new SpectreView.Jdx.Decompressor();
+
 	var tokens = ['599.860','@','V','K','T','%','T','L','k','j','%','J','%','K','L','J','%','n','j','K','j','L','%','k','L','%','j','J','U','L','J','%','k','L','K','1','%','l','L','M','N','P','N','P','R','L','J','0','Q','T','O','J','1','P'];
 	var expected = ['599.860',0,0,0,0,2,4,4,4,7,5,4,4,5,5,7,10,11,11,6,5,7,6,9,9,7,10,10,9,10,11,12,15,16,16,14,17,38,38,35,38,42,47,54,59,66,75,78,88,96,104,110,121,128];
 	
-	var values = jdxReader.processLine(tokens);
+	var values = jdxDecompressor.process(tokens);
 	
 	deepEqual(values, expected);
 });
 
+test('compute', function () {
+	var jdxDecompressor = new SpectreView.Jdx.Decompressor(1.0, 0.001, 1);
+
+	var tokens = ['599.860',0,0,0,0,2,4,4,4,7,5,4,4,5,5,7,10,11,11,6,5,7,6,9,9,7,10,10,9,10,11,12,15,16,16,14,17,38,38,35,38,42,47,54,59,66,75,78,88,96,104,110,121,128];
+	var expected = [[599.86,0],[600.86,0],[601.86,0],[602.86,0],[603.86,0.002],[604.86,0.004],[605.86,0.004],[606.86,0.004],[607.86,0.007],[608.86,0.005],[609.86,0.004],[610.86,0.004],[611.86,0.005],[612.86,0.005],[613.86,0.007],[614.86,0.01],[615.86,0.011],[616.86,0.011],[617.86,0.006],[618.86,0.005],[619.86,0.007],[620.86,0.006],[621.86,0.009000000000000001],[622.86,0.009000000000000001],[623.86,0.007],[624.86,0.01],[625.86,0.01],[626.86,0.009000000000000001],[627.86,0.01],[628.86,0.011],[629.86,0.012],[630.86,0.015],[631.86,0.016],[632.86,0.016],[633.86,0.014],[634.86,0.017],[635.86,0.038],[636.86,0.038],[637.86,0.035],[638.86,0.038],[639.86,0.042],[640.86,0.047],[641.86,0.054],[642.86,0.059000000000000004],[643.86,0.066],[644.86,0.075],[645.86,0.078],[646.86,0.088],[647.86,0.096],[648.86,0.10400000000000001],[649.86,0.11],[650.86,0.121],[651.86,0.128]];
+	
+	var points = jdxDecompressor.compute(tokens);
+	
+	deepEqual(points, expected);
+});
