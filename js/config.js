03f6b9d0684c7d5a534b9d1f7a30008813f301cd
Chase <=>
2013-03-20 22:43:00 -0400

    jqChart includes; Changes to spectrum;
    Getting spectrum to create charts and including the jqChart script and
    css includes

---
+++ js/config.js
@@ -0,0 +1,3 @@
+var config = {
+	//TODO: Config options
+};
--- js/spectreview.spectrum.js
+++ js/spectreview.spectrum.js
@@ -1,17 +1,15 @@
 (function ( SpectreView, $, undefined ) {
 
 	SpectreView.Spectrum = function () {
-		var defaults = {
-			
-		};
+
 	
 		/*
 			old options
 		
-			line color
-			background color
-			plot color
-			integration color
+			line color X
+			background color X
+			plot color 
+			integration color 
 			zoom color
 			save dimension
 			copy dimension
@@ -37,12 +35,52 @@
 		}
 		
 		function onLoadError (jqXHR, status, error) {
-		
+			
 		}
 	}
 
-	$.fn.spectreView = function () {
-	
+	$.fn.spectreView = function (data) {
+		var defaultOptions = {
+	        title: '',
+	        legend: { visible: false },
+	        border: { strokeStyle: '#6ba851' },
+	        background: "#eee",
+	        animation: { duration: 2 },
+	        tooltips: { type: 'shared' },
+	        padding: 0,
+	        shadows: {
+	            enabled: false
+	        },
+	        crosshairs: {
+	            enabled: true,
+	            hLine: false,
+	            vLine: { strokeStyle: '#cc0a0c' }
+	        },
+	        axes: [
+	                {
+	                    location: 'bottom',
+	                    zoomEnabled: true
+	                },
+	                {
+						type: 'linear',
+						location: 'left',
+						labels: { visible: false },
+						majorTickMarks: { visible: false },
+						majorGridLines: { visible: true },
+						strokeStyle: "transparent",
+						lineWidth: 0
+					}
+	              ],
+	        series: [
+	                    {
+				          type: 'line',
+				          data: data,
+				          markers: null
+				      	}
+	                ]
+	    };
+
+		var chart = $(this).jqChart(defaultOptions);
 	};
 	
-}( window.SpectreView = window.SpectreView || {}, jQuery ));
+}( window.SpectreView = window.SpectreView || {}, jQuery));
